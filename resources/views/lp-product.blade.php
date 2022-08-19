13c79f929152c1654207a1ae94a748777d6e17a1
Carlos Maita <carlosmaita2009@gmail.com>
2022-08-19 12:41:16 -0400

    ajustes

--- resources/views/lp-product.blade.php
+++ resources/views/lp-product.blade.php
@@ -11,7 +11,7 @@
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="og:product">
     <meta property="og:url" content="https://rintegrate.com/{{$p->interest->slug}}/{{$p->slug}}">
-    <meta property="og:title" content="{{ucfirst($p->name)}}">
+    <meta property="og:title" content="{{ucfirst(strtolower($p->name))}}">
     <meta property="og:description" content="{{$p->description}}. Encuentra 🕵️ productos Increíbles 😎. Las mejores 🏷️promociones 🏷️. Envio Incluido en todas las compras 📦🛍️ Nuevo Centro de Compras Digital. ">
     <meta property="og:image" content="{{Storage::url($p->image_main)}}">
 
@@ -28,7 +28,7 @@
     <meta property="product:condition" content="new">
     <meta property="product:availability" content="in stock">
     <meta property="product:retailer_item_id" content="{{$p->slug}}_000{{$p->id}}">
-    <meta property="product:item_group_id" content="fb_tshirts">
+    <meta property="product:fb_product_category" content="fb_tshirts">
 
 @endsection
 @php
